35d79e47b8507f070853fb1f4824c5517fef7886
Diogo Dieger <diogaodieger@gmail.com>
2024-03-12 19:39:42 -0300

    fixed slide bug

--- dei-de-presente-lp/src/components/Carousel/index.tsx
+++ dei-de-presente-lp/src/components/Carousel/index.tsx
@@ -1,23 +1,23 @@
-import SwiperCore, {
-  default as Navigation,
-  default as Pagination,
-} from "swiper";
-import "swiper/css";
-import "swiper/css/navigation";
-import "swiper/css/pagination";
-import { Swiper, SwiperSlide } from "swiper/react";
-
-SwiperCore.use([Navigation, Pagination]);
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+import { Navigation, Pagination } from 'swiper/modules';
+import { Swiper, SwiperSlide } from 'swiper/react';
 
 const ImageCarousel: React.FC = () => {
   return (
     <Swiper
       modules={[Navigation, Pagination]}
       spaceBetween={50}
-      slidesPerView={1}
+      slidesPerView={3}
       navigation
       pagination={{ clickable: true }}
+      loop={true}
+      centeredSlides={true}
     >
+      <SwiperSlide>{<img src="/wedding.svg" />}</SwiperSlide>
+      <SwiperSlide>{<img src="/nightclub.svg" />}</SwiperSlide>
+      <SwiperSlide>{<img src="/birthday.svg" />}</SwiperSlide>
       <SwiperSlide>{<img src="/wedding.svg" />}</SwiperSlide>
       <SwiperSlide>{<img src="/nightclub.svg" />}</SwiperSlide>
       <SwiperSlide>{<img src="/birthday.svg" />}</SwiperSlide>
