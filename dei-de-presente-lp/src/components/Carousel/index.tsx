ba5a5dd006264067268e6e1d1f402fcec8474af2
Leonardo Dorea <leolimadorea@gmail.com>
2024-03-13 12:20:46 -0300

    swiper working

--- dei-de-presente-lp/src/components/Carousel/index.tsx
+++ dei-de-presente-lp/src/components/Carousel/index.tsx
@@ -1,7 +1,8 @@
 import "swiper/css";
+import "swiper/css/effect-coverflow";
 import "swiper/css/navigation";
 import "swiper/css/pagination";
-import { Navigation, Pagination } from "swiper/modules";
+import { EffectCoverflow, Navigation, Pagination } from "swiper/modules";
 import { Swiper, SwiperSlide } from "swiper/react";
 import styles from "./styles.module.scss";
 
@@ -9,13 +10,42 @@ const ImageCarousel: React.FC = () => {
   return (
     <div className={styles.container}>
       <Swiper
-        modules={[Navigation, Pagination]}
-        spaceBetween={50}
         slidesPerView={3}
+        spaceBetween={0}
+        modules={[Navigation, Pagination, EffectCoverflow]}
+        effect="coverflow"
         navigation
-        pagination={{ clickable: true }}
-        loop={true}
+        pagination={{
+          clickable: true,
+          el: ".swiper-pagination",
+          type: "bullets",
+        }}
         centeredSlides={true}
+        coverflowEffect={{
+          rotate: 0,
+          stretch: 0,
+          depth: 200,
+          modifier: 1,
+          slideShadows: false,
+        }}
+        breakpoints={{
+          640: {
+            slidesPerView: 1,
+            spaceBetween: 20,
+          },
+          768: {
+            slidesPerView: 3,
+            spaceBetween: 40,
+          },
+          1024: {
+            slidesPerView: 3,
+            spaceBetween: 50,
+          },
+          1440: {
+            slidesPerView: 3,
+            spaceBetween: 50,
+          },
+        }}
       >
         <SwiperSlide>{<img src="/wedding.svg" />}</SwiperSlide>
         <SwiperSlide>{<img src="/nightclub.svg" />}</SwiperSlide>
