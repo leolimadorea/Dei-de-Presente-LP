14e948350e7acbf29f0447f9b6dbfd85295bf582
Diogo Dieger <diogaodieger@gmail.com>
2024-03-13 11:33:04 -0300

    fix: swiper

--- dei-de-presente-lp/src/components/Carousel/index.tsx
+++ dei-de-presente-lp/src/components/Carousel/index.tsx
@@ -1,26 +1,55 @@
 import 'swiper/css';
 import 'swiper/css/navigation';
 import 'swiper/css/pagination';
-import { Navigation, Pagination } from 'swiper/modules';
+import { EffectCoverflow, Navigation } from 'swiper/modules';
 import { Swiper, SwiperSlide } from 'swiper/react';
+import styles from './styles.module.scss';
 
 const ImageCarousel: React.FC = () => {
   return (
     <Swiper
-      modules={[Navigation, Pagination]}
-      spaceBetween={50}
+      modules={[Navigation, EffectCoverflow]}
+      effect={'coverflow'}
       slidesPerView={3}
       navigation
       pagination={{ clickable: true }}
+      coverflowEffect={{
+        rotate: 0,
+        stretch: 0,
+        depth: 100,
+        modifier: 2,
+        slideShadows: true,
+      }}
       loop={true}
       centeredSlides={true}
     >
-      <SwiperSlide>{<img src="/wedding.svg" />}</SwiperSlide>
-      <SwiperSlide>{<img src="/nightclub.svg" />}</SwiperSlide>
-      <SwiperSlide>{<img src="/birthday.svg" />}</SwiperSlide>
-      <SwiperSlide>{<img src="/wedding.svg" />}</SwiperSlide>
-      <SwiperSlide>{<img src="/nightclub.svg" />}</SwiperSlide>
-      <SwiperSlide>{<img src="/birthday.svg" />}</SwiperSlide>
+      <SwiperSlide className={styles.swiperSlide}>
+        <img src="/wedding.svg" alt="Wedding" />
+      </SwiperSlide>
+      <SwiperSlide className={styles.swiperSlide}>
+        <img src="/nightclub.svg" alt="Nightclub" />
+      </SwiperSlide>
+      <SwiperSlide className={styles.swiperSlide}>
+        <img src="/birthday.svg" alt="Birthday" />
+      </SwiperSlide>
+      <SwiperSlide className={styles.swiperSlide}>
+        <img src="/wedding.svg" alt="Wedding again" />
+      </SwiperSlide>
+      <SwiperSlide className={styles.swiperSlide}>
+        <img src="/nightclub.svg" alt="Nightclub again" />
+      </SwiperSlide>
+      <SwiperSlide className={styles.swiperSlide}>
+        <img src="/birthday.svg" alt="Birthday again" />
+      </SwiperSlide>
+      <SwiperSlide className={styles.swiperSlide}>
+        <img src="/wedding.svg" alt="Wedding again" />
+      </SwiperSlide>
+      <SwiperSlide className={styles.swiperSlide}>
+        <img src="/nightclub.svg" alt="Nightclub again" />
+      </SwiperSlide>
+      <SwiperSlide className={styles.swiperSlide}>
+        <img src="/birthday.svg" alt="Birthday again" />
+      </SwiperSlide>
     </Swiper>
   );
 };
